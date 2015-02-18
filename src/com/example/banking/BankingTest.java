fccdba39a7dcc81161a22424e943749abf2df2ba
Doug Bateman <doug@dougbateman.net>
2015-02-18 11:05:50 -0500

    testTransfer - Full test case, code not implemented.

--- src/com/example/banking/BankingTest.java
+++ src/com/example/banking/BankingTest.java
@@ -7,17 +7,24 @@ public class BankingTest {
 	@Test
 	public void testTransfer() {
 		//1. Setup (Assemble)
+		AccountDAO dao = new InMemoryAccountDAO();
 		BankingService teller = new SimpleBankingService();
 		
 		//2. Create test data / test fixture
-		long fromAccountId = 1;
-		long toAccountId = 2;
+		Account from = dao.create("Val 401k", 1_000_000_000.00);
+		Account to = dao.create("Doug 401k", 5.00);
+		long fromAccountId = from.getId();
+		long toAccountId = to.getId();
 		double amount = 1_000_000_000.00;
 		
 		//3. Act (do the business logic)
 		teller.transfer(fromAccountId, toAccountId, amount);
 		
 		//4. Verify the results
+		Account finalFrom = dao.find(fromAccountId);
+		Account finalTo = dao.find(toAccountId);
+		Assert.assertEquals(0.00, finalFrom.getBalance());
+		Assert.assertEquals(1_000_000_000.00, finalTo.getBalance());
 		
 		//5. Cleanup
 	}
