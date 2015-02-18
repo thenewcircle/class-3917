1a07b39147288bb8679082695c8ff8373a5e981a
Doug Bateman <doug@dougbateman.net>
2015-02-18 10:47:30 -0500

    testTransfer - now compiles, and passes, but not implemented.

---
+++ src/com/example/banking/BankingService.java
@@ -0,0 +1,7 @@
+package com.example.banking;
+
+public interface BankingService {
+
+	void transfer(long fromAccountId, long toAccountId, double amount);
+
+}
---
+++ src/com/example/banking/SimpleBankingService.java
@@ -0,0 +1,11 @@
+package com.example.banking;
+
+public class SimpleBankingService implements BankingService {
+
+	@Override
+	public void transfer(long fromAccountId, long toAccountId, double amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
