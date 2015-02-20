30cf914e54b3ec08de86926a7ad5dee1eb102de2
Doug Bateman <doug@dougbateman.net>
2015-02-20 13:11:46 -0500

    MyPackmanTest now passes.

--- src/com/example/pacman/MyPacmanTest.java
+++ src/com/example/pacman/MyPacmanTest.java
@@ -1,20 +1,44 @@
 package com.example.pacman;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 
 public class MyPacmanTest {
 
 	@Test
-	public void foo() {
-		CustomerService x;
-		DistributionEngine x2;
-		MyMessageDrivenBean x3;
-		MyStatelessWorkerBean x4;
-		MyValidator x5;
-		NetworkAddrService x6;
-		RequestMessageLogService x7;
-		SecurityService x8;
-		ValidationError x9;
+	public void testMyValidator() {
+		//1. Test Data
+		String json = "Doug, Jacob, Mike";
+		RequestMessage requestMessage = RequestMessage.parse(json);
+		List<ValidationError> expectedErrors = Arrays.asList(
+				new ValidationError("Doug", 7032),
+				new ValidationError("Jacob", 7032),
+				new ValidationError("Mike", 7032)
+		);
+		
+		//2. Assemble
+		CustomerService customerService = Mockito.mock(CustomerService.class);
+		NetworkAddrService networkAddrService = Mockito.mock(NetworkAddrService.class);
+		RequestMessageLogService requestMessageLogService = Mockito.mock(RequestMessageLogService.class);
+		Mockito.when(customerService.findCustomer()).thenReturn("Doug");
+		MyValidator validator = new MyValidator();
+		validator.setCustomerService(customerService);
+		validator.setNetworkAddrService(networkAddrService);
+		validator.setRequestMessageLogService(requestMessageLogService);
+		
+		//3. Act (Do business logic)
+		List<ValidationError> actualErrors = validator.validate(requestMessage);
+		
+		//4. Assert/Verify
+		Assert.assertEquals(expectedErrors, actualErrors);
+		verify(requestMessageLogService, times(3)).log((ValidationError) anyObject());
+		
+		//5. Clean Up
 	}
 	
 }
